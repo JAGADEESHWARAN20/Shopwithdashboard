10c4e77f68f430a91233be3bba7f7667d7c4612a
JAGADEESHWARAN20 <jagadeeshwaransp5@gmail.com>
2024-03-06 22:59:47 +0530

    new files created and updated

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,23 @@
 import { authMiddleware } from "@clerk/nextjs";
- 
-// This example protects all routes including api/trpc routes
-// Please edit this to allow other routes to be public as needed.
-// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
-export default authMiddleware({});
- 
+
+// Configure the public and ignored routes as needed
+export default authMiddleware({
+  // Define the routes that can be accessed while signed out
+  publicRoutes: [
+    "/",
+    "/about",
+    // Add more public routes here as needed
+  ],
+  // Define routes that should be ignored by the authentication middleware
+  ignoredRoutes: [
+    // Add routes that should be ignored here
+  ],
+});
+
+// Configure the matcher to protect all routes except the public ones
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  // Protects all routes, including api/trpc.
+  // See https://clerk.com/docs/references/nextjs/auth-middleware
+  // for more information about configuring your Middleware
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)"],
 };
