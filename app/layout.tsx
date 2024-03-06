358b74eb3bafe836d548c2c1e357191f0eb1f51b
JAGADEESHWARAN20 <jagadeeshwaransp5@gmail.com>
2024-03-06 11:16:11 +0530

    init files adding to repo

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from '@clerk/nextjs' 
+import { ModalProvider } from "@/Providers/modal-provider";
+import { ToasterProvider } from "@/Providers/toast-provider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Admin Dashboard",
+  description: "Admin Dashboard",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+         <body className={inter.className}>
+          <ToasterProvider/>
+          <ModalProvider/>
+          {children}
+         </body>
+      </html>
+    </ClerkProvider>
+  );
+}
