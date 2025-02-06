759a8a7e379aa4d7963d71f03c5191bf4d709062
JAGADEESHWARAN <66456490+JAGADEESHWARAN20@users.noreply.github.com>
2025-02-06 15:26:42 +0530

    Delete app directory

--- app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+++
@@ -1,6 +0,0 @@
-'use client'
-import { SignIn } from "@clerk/nextjs";
-
-export default function Page() {
-  return <SignIn path="/sign-in" fallbackRedirectUrl="/dashboard" />;
-}
--- app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
+++
@@ -1,6 +0,0 @@
-'use client'
-import { SignUp } from "@clerk/nextjs";
-
-export default function Page() {
-  return <SignUp fallbackRedirectUrl="/dashboard" path="/sign-up" />;
-}
