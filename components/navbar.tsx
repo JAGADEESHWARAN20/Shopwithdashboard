133c417b7b6602f5499c9ed41fed3c10763cd628
JAGADEESHWARAN20 <jagadeeshwaransp5@gmail.com>
2025-02-06 14:29:36 +0530

    New Updates Till Orders

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -24,8 +24,8 @@ const Navbar = async () => {
     return (
         <div className='border-b'>
             <div className="flex  h-16 items-center px-4">
-                <StoreSwitcher items={stores} />
                 <MainNav className='mx-6 ' />
+                <StoreSwitcher items={stores} />
                 <div className="ml-auto flex items-center space-x-4">
                     <UserButton afterSignOutUrl='/' />
                 </div>
