019eb92a6bfc8ee6b9a4a281652140a176f7d099
JAGADEESHWARAN20 <jagadeeshwaransp5@gmail.com>
2025-03-17 16:45:50 +0530

    backend ready to use

--- ecommerce-store/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import HomePage from "./(routes)/page";
-
-export default function Home() {
-  return (
-    <div className="">
-      <HomePage />
-    </div>
-  );
-}
--- ecommerce-store/components/ui/no-results.tsx
+++
@@ -1,9 +0,0 @@
-const NoResults = () => {
-     return (
-          <div className="text-neutral-500 flex items-center justify-center w-full h-full">
-               You have No products in Store
-          </div>
-     )
-}
-
-export default NoResults;
