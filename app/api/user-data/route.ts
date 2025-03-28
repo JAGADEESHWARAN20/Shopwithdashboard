730e20272821806a402f254ce4bbe4ce4336d473
JAGADEESHWARAN20 <jagadeeshwaransp5@gmail.com>
2025-03-28 22:11:26 +0530

    Fixed TypeScript errors and implemented dynamic Navbar with categories and store name

---
+++ app/api/user-data/route.ts
@@ -0,0 +1,16 @@
+// app/api/user-data/route.ts
+import { users } from "@clerk/backend";
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export async function GET(req: Request) {
+    const { userId } = await auth();
+
+    if (!userId) {
+        return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const user = await users.getUser(userId);
+
+    return NextResponse.json(user);
+}
