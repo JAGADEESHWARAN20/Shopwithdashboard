a1e907c6b0f23468471d7a018d0846162d8eb7b9
JAGADEESHWARAN20 <jagadeeshwaransp5@gmail.com>
2025-04-08 14:58:16 +0530

    new updates in api

---
+++ app/api/user-added/route.ts
@@ -0,0 +1,30 @@
+import { PrismaClient } from "@prisma/client";
+import { NextResponse, NextRequest } from "next/server";
+
+const prisma = new PrismaClient();
+
+export async function POST(req: NextRequest) {
+     try {
+          const { userId, name } = await req.json();
+
+          if (!userId || !name) {
+               return NextResponse.json({ error: "Missing userId or name." }, { status: 400 });
+          }
+
+          const updatedUser = await prisma.user.update({
+               where: {
+                    id: userId,
+               },
+               data: {
+                    name: name,
+               },
+          });
+
+          return NextResponse.json({ message: "User details updated successfully!", user: updatedUser }, { status: 200 });
+     } catch (error: any) {
+          console.error("Error updating user details:", error);
+          return NextResponse.json({ error: "Failed to update user details." }, { status: 500 });
+     } finally {
+          await prisma.$disconnect();
+     }
+}
