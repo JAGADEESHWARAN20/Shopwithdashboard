74d3e293c12d26f38d0c2d67aada71017d25f5af
JAGADEESHWARAN <66456490+JAGADEESHWARAN20@users.noreply.github.com>
2025-04-13 08:29:24 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -13,16 +13,15 @@ const nextConfig = {
   async headers() {
     return [
       {
-       
         source: "/api/:path*",
         headers: [
-          { key: "Access-Control-Allow-Origin", value: "http://localhost:3002" },
+          { key: "Access-Control-Allow-Origin", value: "https://ecommercestore-online.vercel.app, https://kajol-ecommercestore-online.vercel.app" },
           { key: "Access-Control-Allow-Methods", value: "GET, POST, PUT, DELETE, OPTIONS" },
           { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" },
           { key: "Access-Control-Allow-Credentials", value: "true" },
         ],
       },
-    ]
+    ];
   },
 };
 
