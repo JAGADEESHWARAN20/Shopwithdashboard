83cdcd4b4dc8effbe1d88a1a4f0fc40a53528095
evidancemakerphotoeditor <evidancemakerphotoeditor@gmail.com>
2026-04-04 11:30:30 +0530

    new data

--- app/api/[storeId]/design-collections/count/route.ts
+++ app/api/[storeId]/design-collections/count/route.ts
@@ -3,10 +3,14 @@ import { NextResponse } from "next/server";
 
 export async function GET(_: Request, { params }: any) {
   const count = await prismadb.designCollection.count({
-    where: {
-      storeId: params.storeId,
-    },
+    where: { storeId: params.storeId },
   });
 
-  return NextResponse.json(count);
+  const response = NextResponse.json(count);
+  
+  // Add these lines to fix the "Fetch failed" errors in the browser
+  response.headers.set("Access-Control-Allow-Origin", "*"); 
+  response.headers.set("Access-Control-Allow-Methods", "GET, OPTIONS");
+  
+  return response;
 }
