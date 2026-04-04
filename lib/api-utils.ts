fd6132b105a5f2d44c1c65af0a4d3ad14137b000
evidancemakerphotoeditor <evidancemakerphotoeditor@gmail.com>
2026-04-04 16:32:27 +0530

    new update

--- lib/api-utils.ts
+++ lib/api-utils.ts
@@ -1,6 +1,6 @@
 export function getCorsHeaders(origin: string | null) {
     return {
-      "Access-Control-Allow-Origin": origin || "*",
+      "Access-Control-Allow-Origin": origin || "https://nwtailormadestudio.vercel.app",
       "Access-Control-Allow-Methods": "GET, POST, PATCH, DELETE, OPTIONS",
       "Access-Control-Allow-Headers": "Content-Type, Authorization",
     };
--- middleware.ts
+++ middleware.ts
@@ -43,7 +43,7 @@ export default clerkMiddleware((auth, request) => {
   if (origin && allowedOrigins.includes(origin)) {
     response.headers.set('Access-Control-Allow-Origin', origin);
   } else {
-    response.headers.set('Access-Control-Allow-Origin', '*'); 
+    response.headers.set('Access-Control-Allow-Origin', 'https://nwtailormadestudio.vercel.app'); 
   }
 
   response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
