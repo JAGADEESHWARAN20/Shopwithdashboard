89a1606b4638e0b1ae67e0bb1d8b9655c78c73ab
JAGADEESHWARAN <66456490+JAGADEESHWARAN20@users.noreply.github.com>
2026-04-05 13:38:24 +0530

    Update route.ts

--- app/api/stores/[storeId]/route.ts
+++ app/api/stores/[storeId]/route.ts
@@ -4,7 +4,7 @@ import { getAuth } from "@clerk/nextjs/server";
 
 // Utility function to set CORS headers
 function setCorsHeaders(response: NextResponse) {
-    response.headers.set("Access-Control-Allow-Origin", "*");
+    response.headers.set("Access-Control-Allow-Origin", "https://nwtailormadestudio.vercel.app");
     response.headers.set("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
     response.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
     return response;
