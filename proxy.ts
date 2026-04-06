b9b25d601582e11eeebcf495125f060d24d194c7
JAGADEESHWARAN <jagadeeshwaransp5@gmail.com>
2026-04-06 15:00:30 +0530

    new update

--- proxy.ts
+++ proxy.ts
@@ -15,7 +15,7 @@ export default clerkMiddleware((auth, request) => {
   }
 
   if (!isPublicRoute(request)) {
-    auth().protect();
+    auth.protect();
   }
 
   const response = NextResponse.next();
