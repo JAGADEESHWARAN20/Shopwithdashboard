cc96eac7f8c8d1cda324fea037dfb8087cd7843c
JAGADEESHWARAN <jagadeeshwaransp5@gmail.com>
2026-04-08 19:28:06 +0530

    new update

--- components/ui/command.tsx
+++ components/ui/command.tsx
@@ -24,9 +24,7 @@ const Command = React.forwardRef<
 ))
 Command.displayName = CommandPrimitive.displayName
 
-interface CommandDialogProps extends DialogProps {
-  children?: React.ReactNode;
-}
+type CommandDialogProps = React.ComponentProps<typeof Dialog>;
 
 
 const CommandDialog = ({ children, ...props }: CommandDialogProps) => {
@@ -34,7 +32,7 @@ const CommandDialog = ({ children, ...props }: CommandDialogProps) => {
     <Dialog {...props}>
       <DialogContent className="overflow-hidden p-0 shadow-lg">
         <Command className="[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5">
-          {children}
+          {children as React.ReactNode}
         </Command>
       </DialogContent>
     </Dialog>
