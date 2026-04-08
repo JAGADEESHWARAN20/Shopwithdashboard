ad62c2b8818b560ce1397edb7ad4b8b58f3e649b
JAGADEESHWARAN20 <jagadeeshwaransp5@gmail.com>
2026-04-08 23:13:22 +0530

    Initial commit

--- middleware.ts
+++ middleware.ts
@@ -7,10 +7,14 @@ const isPublicRoute = createRouteMatcher([
   "/api/(.*)",
 ]);
 
-export default clerkMiddleware((auth, req) => {
-  // protect non-public routes
+export default clerkMiddleware(async (auth, req) => {
   if (!isPublicRoute(req)) {
-    auth.protect();
+    const { userId } = await auth();
+
+    if (!userId) {
+      const signInUrl = new URL("/sign-in", req.url);
+      return NextResponse.redirect(signInUrl);
+    }
   }
 
   return NextResponse.next();
@@ -18,6 +22,7 @@ export default clerkMiddleware((auth, req) => {
 
 export const config = {
   matcher: [
+    "/",
     "/((?!_next|.*\\..*).*)",
     "/(api|trpc)(.*)",
   ],
